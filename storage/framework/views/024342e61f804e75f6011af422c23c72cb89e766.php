1d89b92e485f6468476a8ef87ae0972dc395f063
rsuregar <rsuregar@gmail.com>
2021-09-13 07:23:22 +0800

    Laravel 8 SSO Server

---
+++ storage/framework/views/024342e61f804e75f6011af422c23c72cb89e766.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.14.0/dist/full.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body>
+    <?php echo $__env->yieldContent('content'); ?>
+</body>
+</html>
+<?php /**PATH D:\laragon\www\sso\resources\views/tailwinds/layouts/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4e458e1eccdff06656d3f89d22a7495a27e779ec.php
@@ -0,0 +1,31 @@
+<?php $__env->startSection('content'); ?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"><?php echo e(__('Dashboard')); ?></div>
+
+                <div class="card-body">
+                    <?php if(session('status')): ?>
+                        <div class="alert alert-success" role="alert">
+                            <?php echo e(session('status')); ?>
+
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo e(__('You are logged in!')); ?>
+
+
+                    <ol>
+                        <?php $__currentLoopData = \Laravel\Passport\Client::wherePersonalAccessClient(false)->wherePasswordClient(false)->get(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><a href="<?php echo e($item->redirect); ?>" target="_blank"><?php echo e($item->name); ?></a></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laragon\www\sso\resources\views/home.blade.php ENDPATH**/ ?>
