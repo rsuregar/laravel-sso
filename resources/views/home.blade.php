92c9f2302bf13fed221f827d798118c9abec93b4
rsuregar <rsuregar@gmail.com>
2021-09-13 07:27:13 +0800

    Laravel 8 SSO Server

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+
 
                     <ol>
                         @foreach (\Laravel\Passport\Client::wherePersonalAccessClient(false)->wherePasswordClient(false)->get() as $item)
